7ef807113aa5d714bffdf9436b8066b5c3ca715d
El Mahdi Abouraouf <eabourao@e3r11p5.1337.ma>
2025-09-05 18:25:11 +0100

    asd

--- habit_tracker/script.js
+++
@@ -1,7 +0,0 @@
-let obj = {
-	"fname": "issam",
-	"lname": "guellouch",
-	"userName": "is4545",
-	"email": "email@gmail.com", //regex
-	"password": "life is good" //force 8chars, use upper lower 
-}
